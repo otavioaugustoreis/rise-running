dea8274023b7ae3eb1cf8bc723085b4762d742f9
Otavbio Augusto Reis Almeida <oaugusto265@gmail.com>
2025-11-13 13:26:44 -0300

    fix: ajustando request

--- RiseRunning-ScannerCode/Controllers/RunnersController.cs
+++ RiseRunning-ScannerCode/Controllers/RunnersController.cs
@@ -12,35 +12,34 @@ namespace RiseRunning_ScannerCode.Controllers
     [Route("[controller]")]
     public class RunnersController(IMemoryCache memoryCache) : ControllerBase
     {   
-
-        private readonly ConcurrentQueue<RunnerEntity> _runners = new();
         private readonly IMemoryCache _memoryCache = memoryCache;
         private const string CacheKey = "RunnersQueue";
-
 
         [HttpPost]
         public async Task<IActionResult> PostRunner([FromHeader] string nome, [FromHeader] string cpf)
         {
             if (!ValidateCPF.IsCpf(cpf)) 
                      Ok(MessageCommons.RunnerCpf);
-           
-                foreach (var item in _runners)
-                {
-                    if (item.Cpf == ValidateCPF.cpfToLong(cpf))
-                    {
-                        return Ok(MessageCommons.RunnerJaRegistrado(item.Nome));
-                    }
-                }
 
-                var runner = new RunnerEntity
+            if (!_memoryCache.TryGetValue(CacheKey, out ConcurrentQueue<RunnerEntity> runners))
+            {
+                runners = new ConcurrentQueue<RunnerEntity>();
+            }
+
+            if (runners!.Any(r => r.Cpf == ValidateCPF.cpfToLong(cpf)))
+            {
+                var existente = runners.First(r => r.Cpf == ValidateCPF.cpfToLong(cpf));
+                return Ok(MessageCommons.RunnerJaRegistrado(existente.Nome));
+            }
+            var runner = new RunnerEntity
             {
                 Nome = nome,
                 Cpf = ValidateCPF.cpfToLong(cpf)
             };
 
-            _runners.Enqueue(runner);
+            runners.Enqueue(runner);
 
-            _memoryCache.Set(CacheKey, _runners, TimeSpan.FromMinutes(30));
+            _memoryCache.Set(CacheKey, runners, TimeSpan.FromMinutes(30));
 
             return Ok(MessageCommons.RunnerRegistrado(runner.Nome));
         }
@@ -48,14 +47,14 @@ namespace RiseRunning_ScannerCode.Controllers
         [HttpGet]
         public ActionResult<List<RunnerEntity>> GetAllRunner() 
         {
-            //if (_memoryCache.TryGetValue(CacheKey, out ConcurrentQueue<RunnerEntity> runners))
-            //{
-            //    return runners!
-            //        .OrderByDescending(p => p.DataHora)
-            //        .ToList();
-            //}
+            if (_memoryCache.TryGetValue(CacheKey, out ConcurrentQueue<RunnerEntity> runners))
+            {
+                return runners!
+                    .OrderByDescending(p => p.DataHora)
+                    .ToList();
+            }
 
-            return _runners
+            return runners
                 .OrderByDescending(p => p.DataHora)
                 .ToList();
         }
